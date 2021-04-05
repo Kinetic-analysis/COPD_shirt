102dbee00a29079aeca245733e5eeb3b8b337d47
JackyWongKA <jackywong@kinetic-analysis.com>
2021-04-05 14:51:41 +0200

    Data opslag in txt bestand

--- Python/Data_Save.py
+++ Python/Data_Save.py
@@ -1,23 +1,33 @@
-import socket
+from datetime import datetime
+import string
+import random
+import time
 
-HOST = '192.168.192.42' # accept any IP-adres
-PORT = 2000 # Port to listen on
-bufferSize  = 1024
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
+# datetime object containing current date and time
+now = datetime.now()
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    while True:
-        bytesAddressPair = s.recvfrom(bufferSize)
-        message = bytesAddressPair[0]
-        address = bytesAddressPair[1]
+# dd/mm/YY H:M:S
+dt_String = now.strftime("%d/%m/%Y %H:%M:%S")
+file1 = open("COPD_Data.txt","a")
+file1.write(dt_String)
+file1.write("\n")
+file1.close()
 
-        clientMsg = "Message from Client:{}".format(message)
-        #clientIP  = "Client IP Address:{}".format(address)
-        
-        print(clientMsg)
-        #print(clientIP)
-
-        # Sending a reply to client
-        #s.sendto(bytesToSend, address)
+i = 1
+while i > 0:
+    tijd_String = ''.join(random.choices(string.digits, k = 10))  
+    cap_String = ''.join(random.choices(string.digits, k = 10))
+    res_String = ''.join(random.choices(string.digits, k = 10))
+    
+    file1 = open("COPD_Data.txt","a")
+    file1.write("Tijd van meting: ")
+    file1.write(tijd_String)
+    file1.write("\t")
+    file1.write("Capcitieve Stretch sensor: ")
+    file1.write(cap_String)
+    file1.write("\t")
+    file1.write("Resistieve Stretch sensor: ")
+    file1.write(res_String)
+    file1.write("\n")
+    file1.close()
+    time.sleep(1)
