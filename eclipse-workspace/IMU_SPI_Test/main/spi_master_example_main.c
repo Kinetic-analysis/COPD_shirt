ae52779e1da534625a1760e140fd3230e5db43bf
JackyWongKA <jackywong@kinetic-analysis.com>
2021-04-29 17:48:33 +0200

    IMU Accelero+Gyro werkend

--- eclipse-workspace/IMU_SPI_Test/main/spi_master_example_main.c
+++ eclipse-workspace/IMU_SPI_Test/main/spi_master_example_main.c
@@ -60,7 +60,16 @@ void beginSPI(spi_device_handle_t spi)
 
 }
 
-
+int16_t TwosComplement_To_Integer(uint16_t integer)
+{
+	int16_t value = integer;
+    if((integer >> 15) == 0b0000000000000001) //if its negative
+    {
+    	value = ~integer;
+    	value = (integer + 1)*-1;
+    }
+    return value;
+}
 
 bool IMU_write_reg(spi_device_handle_t spi, uint8_t reg, uint8_t data)
 {
@@ -108,17 +117,135 @@ void IMU_read_ID(spi_device_handle_t spi)
 	uint8_t reg = 0x00;
 	uint8_t id;
 
+	uint8_t write;
+	uint8_t read;
+
+	//write REG_BANK_SEL
+	reg = 0x7F;
+	write = 0x00;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
+
+	reg = 0x00;
 	IMU_read_reg(spi, reg, &id);
 	printf("IMU ID: 0x%02X\n", id);
 }
 
+void IMU_read_gyro_registers(spi_device_handle_t spi)
+{
+	uint8_t reg;
+	uint8_t read;
+	uint8_t write;
+
+	reg = 0x7F;
+	write = 0x20;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
+
+	reg = 0x01;
+	write = 0x01;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("GYRO_CONFIG_1: 0x%02X\n", read);
+
+	reg = 0x7F;
+	write = 0x00;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
+}
+
 void IMU_read_ID2(spi_device_handle_t spi)
 {
-	uint8_t reg = 0x01;
-	uint8_t id;
+	uint8_t reg;
+	uint8_t read;
+	uint8_t write;
 
-	IMU_read_reg(spi, reg, &id);
-	printf("IMU ID: 0x%02X\n", id);
+	//write REG_BANK_SEL
+	reg = 0x7F;
+	write = 0x00;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
+
+	//write USER_CTRL
+	reg = 0x03;
+	write = 0x20;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("USER_CTRL: 0x%02X\n", read);
+
+	//write REG_BANK_SEL
+	reg = 0x7F;
+	write = 0x30;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
+
+	//write I2C_MST_CTRL
+	reg = 0x01;
+	write = 0x17;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_MST_CTRL: 0x%02X\n", read);
+
+	//write I2C_MST_DELAY_CTRL
+	reg = 0x02;
+	write = 0x01;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_MST_DELAY_CTRL: 0x%02X\n", read);
+
+	//write I2C_SLV0_ADDR
+	reg = 0x03;
+	write = 0x0C;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_ADDR: 0x%02X\n", read);
+
+	//write I2C_SLV0_REG
+	reg = 0x04;
+	write = 0x31;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_REG: 0x%02X\n", read);
+
+	//write I2C_SLV0_DO
+	reg = 0x06;
+	write = 0x08;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_REG: 0x%02X\n", read);
+
+	//write I2C_SLV0_CTRL
+	reg = 0x05;
+	write = 0x8A;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_CTRL: 0x%02X\n", read);
+
+	//write I2C_SLV0_ADDR
+	reg = 0x03;
+	write = 0x8C;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_ADDR: 0x%02X\n", read);
+
+	//write I2C_SLV0_REG
+	reg = 0x04;
+	write = 0x11;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("I2C_SLV0_REG: 0x%02X\n", read);
+
+	//write REG_BANK_SEL
+	reg = 0x7F;
+	write = 0x00;
+	IMU_write_reg(spi, reg, write);
+	IMU_read_reg(spi, reg, &read);
+	printf("REG_BANK_SEL: 0x%02X\n", read);
 }
 
 void IMU_read_data(spi_device_handle_t spi)
@@ -142,33 +269,24 @@ void IMU_read_data(spi_device_handle_t spi)
 	uint8_t Y_GYRO_OUT_L;
 	uint8_t Z_GYRO_OUT_H;
 	uint8_t Z_GYRO_OUT_L;
-	uint8_t X_MAGN_OUT_H;
-	uint8_t X_MAGN_OUT_L;
-	uint8_t Y_MAGN_OUT_H;
-	uint8_t Y_MAGN_OUT_L;
-	uint8_t Z_MAGN_OUT_H;
-	uint8_t Z_MAGN_OUT_L;
-	uint16_t TEMP_OUT = 0x0000;
-	uint16_t X_ACCEL_OUT = 0x0000;
-	uint16_t Y_ACCEL_OUT = 0x0000;
-	uint16_t Z_ACCEL_OUT = 0x0000;
-	uint16_t X_GYRO_OUT = 0x0000;
-	uint16_t Y_GYRO_OUT = 0x0000;
-	uint16_t Z_GYRO_OUT = 0x0000;
-	uint16_t X_MAGN_OUT = 0x0000;
-	uint16_t Y_MAGN_OUT = 0x0000;
-	uint16_t Z_MAGN_OUT = 0x0000;
 
-	float Temp_finalwaarde;
-	float X_ACCEL_finalwaarde;
-	float Y_ACCEL_finalwaarde;
-	float Z_ACCEL_finalwaarde;
+	int16_t TEMP_OUT;
+	int16_t X_ACCEL_OUT;
+	int16_t Y_ACCEL_OUT;
+	int16_t Z_ACCEL_OUT;
+	int16_t X_GYRO_OUT;
+	int16_t Y_GYRO_OUT;
+	int16_t Z_GYRO_OUT;
+
+
+	float Temp_finalwaarde = 0;
+	float X_ACCEL_finalwaarde = 0;
+	float Y_ACCEL_finalwaarde = 0;
+	float Z_ACCEL_finalwaarde = 0;
 	float X_GYRO_finalwaarde;
 	float Y_GYRO_finalwaarde;
 	float Z_GYRO_finalwaarde;
-	float X_MAGN_finalwaarde;
-	float Y_MAGN_finalwaarde;
-	float Z_MAGN_finalwaarde;
+
 
 	reg1 = 0x39;
 	reg2 = 0x3A;
@@ -200,37 +318,14 @@ void IMU_read_data(spi_device_handle_t spi)
 	reg2 = 0x38;
 	IMU_read_reg(spi, reg1, &Z_GYRO_OUT_H);
 	IMU_read_reg(spi, reg2, &Z_GYRO_OUT_L);
-//	reg1 = 0x31;
-//	reg2 = 0x32;
-//	IMU_read_reg(spi, reg1, &X_MAGN_OUT_H);
-//	IMU_read_reg(spi, reg2, &X_MAGN_OUT_L);
-//	reg1 = 0x31;
-//	reg2 = 0x32;
-//	IMU_read_reg(spi, reg1, &Y_MAGN_OUT_H);
-//	IMU_read_reg(spi, reg2, &Y_MAGN_OUT_L);
-//	reg1 = 0x31;
-//	reg2 = 0x32;
-//	IMU_read_reg(spi, reg1, &Z_MAGN_OUT_H);
-//	IMU_read_reg(spi, reg2, &Z_MAGN_OUT_L);
 
-
-	TEMP_OUT = (TEMP_OUT_H << 8);
-	TEMP_OUT |= TEMP_OUT_L;
-	X_ACCEL_OUT = (X_ACCEL_OUT_H << 8);
-	X_ACCEL_OUT |= X_ACCEL_OUT_L;
-	Y_ACCEL_OUT = (Y_ACCEL_OUT_H << 8);
-	Y_ACCEL_OUT |= Y_ACCEL_OUT_L;
-	Z_ACCEL_OUT = (Z_ACCEL_OUT_H << 8);
-	Z_ACCEL_OUT |= Z_ACCEL_OUT_L;
-	X_GYRO_OUT = (X_GYRO_OUT_H << 8);
-	X_GYRO_OUT |= X_GYRO_OUT_L;
-	Y_GYRO_OUT = (Y_GYRO_OUT_H << 8);
-	Y_GYRO_OUT |= Y_GYRO_OUT_L;
-	Z_GYRO_OUT = (Z_GYRO_OUT_H << 8);
-	Z_GYRO_OUT |= Z_GYRO_OUT_L;
-
-
-
+	TEMP_OUT = ((TEMP_OUT_H << 8) | (TEMP_OUT_L & 0xFF));
+	X_ACCEL_OUT = ((X_ACCEL_OUT_H << 8) | (X_ACCEL_OUT_L & 0xFF));
+	Y_ACCEL_OUT = ((Y_ACCEL_OUT_H << 8) | (Y_ACCEL_OUT_L & 0xFF));
+	Z_ACCEL_OUT = ((Z_ACCEL_OUT_H << 8) | (Z_ACCEL_OUT_L & 0xFF));
+	X_GYRO_OUT = ((X_GYRO_OUT_H << 8) | (X_GYRO_OUT_L & 0xFF));
+	Y_GYRO_OUT = ((Y_GYRO_OUT_H << 8) | (Y_GYRO_OUT_L & 0xFF));
+	Z_GYRO_OUT = ((Z_GYRO_OUT_H << 8) | (Z_GYRO_OUT_L & 0xFF));
 
 	Temp_finalwaarde = ((TEMP_OUT-20)/333.87)+21;
 	X_ACCEL_finalwaarde = X_ACCEL_OUT/16.384;
@@ -283,23 +378,93 @@ spi_device_handle_t SPI_init(void)
     return spi;
 }
 
+void IMU_read_Magneto2(spi_device_handle_t spi)
+{
+	uint8_t reg;
+	uint8_t data;
+	uint8_t read;
+
+
+}
+
+void IMU_read_Magneto2(spi_device_handle_t spi)
+{
+	uint8_t reg;
+	uint8_t data;
+	uint8_t read;
+
+	//read EXT_SLV_SENS_DATA_00
+	reg = 0x3B;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_00: 0x%02X\n", read);
+	reg = 0x3C;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_01: 0x%02X\n", read);
+	reg = 0x3D;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_02: 0x%02X\n", read);
+	reg = 0x3E;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_03: 0x%02X\n", read);
+	reg = 0x3F;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_04: 0x%02X\n", read);
+	reg = 0x40;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_05: 0x%02X\n", read);
+	reg = 0x41;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_06: 0x%02X\n", read);
+	reg = 0x42;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_07: 0x%02X\n", read);
+	reg = 0x43;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_08: 0x%02X\n", read);
+	reg = 0x44;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_09: 0x%02X\n", read);
+	reg = 0x45;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_10: 0x%02X\n", read);
+	reg = 0x46;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_11: 0x%02X\n", read);
+	reg = 0x47;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_12: 0x%02X\n", read);
+	reg = 0x48;
+	IMU_read_reg(spi, reg, &read);
+	printf("EXT_SLV_SENS_DATA_13: 0x%02X\n", read);
+}
+
+
 void app_main(void)
 {
 	uint8_t reg = 0x06;
-	uint8_t data = 0x00;
-
+	uint8_t data = 0x01;
 	spi_device_handle_t spi;
 	spi = SPI_init();
+	IMU_write_reg(spi, reg, data);
     IMU_read_ID(spi);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    IMU_write_reg(spi, reg, data);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-
+    IMU_read_ID2(spi);
     while(1)
     {
-    	//IMU_read_ID(spi);
-    	//IMU_read_data(spi);
-    	IMU_read_ID2(spi);
-    	vTaskDelay(1000 / portTICK_PERIOD_MS);
+    	IMU_read_data(spi);
+    	//IMU_read_Magneto(spi);
+    	vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
